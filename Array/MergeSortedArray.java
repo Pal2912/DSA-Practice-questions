1d8d5438c4688949605d981ff03690e5f75cace7
Pal2912 <120407245+Pal2912@users.noreply.github.com>
2025-02-19 18:03:45 +0530

    add MergeSortedArray.java

---
+++ Array/MergeSortedArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+       
+        for(int i=m,j=0;i<m+n;i++,j++){
+            nums1[i] = nums2[j];
+        }
+        Arrays.sort(nums1);
+        return nums1;
+    }
+}
